13ee73005cc8a88efb8d03463050ac61cac88132
proXDhiya <prox.dhiya@gmail.com>
2022-07-31 19:09:03 +0100

    create simple react exemple

---
+++ Projects/TEAM_08/frontend/src/pages/errorpage.js
@@ -0,0 +1,11 @@
+const Errorpage = () => {
+    return (
+        <div className="error-page">
+            <h1>Error</h1>
+            <p>Page not Found 404</p>
+        </div>
+    );
+};
+
+
+export default Errorpage;
