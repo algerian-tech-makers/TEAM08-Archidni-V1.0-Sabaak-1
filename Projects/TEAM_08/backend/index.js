d6b30aafa67d0439d0ce20683fd4647422a45e50
proXDhiya <prox.dhiya@gmail.com>
2022-07-31 19:08:43 +0100

    change backend port to 4000

--- Projects/TEAM_08/backend/index.js
+++ Projects/TEAM_08/backend/index.js
@@ -21,7 +21,7 @@ app.use('/api/learners', require('./routes/learner'));
 
 
 // set port
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
     console.log('Server is running on port ' + port);
 });
