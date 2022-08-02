73a1f69aad02bbb4d46e9d73792d227ba8e821eb
Samir-SB <samir.belhadi@univ-batna2.dz>
2022-08-02 13:42:54 +0100

    Test  the routes

--- Projects/TEAM_08/frontend/src/App.jsx
+++ Projects/TEAM_08/frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import SchoolList from './pages/SchoolList';
@@ -9,10 +9,35 @@ import UserList from './pages/UserList';
 import UserDetails from './pages/UserDetails';
 import Dashboard from './pages/Dashboard';
 import UserProfile from './pages/UserProfile';
+import Button from '@mui/material/Button';
 
 function App() {
+  // just a simple example to test routes
+  const pages = [
+    { label: 'Home', path: '/' },
+    { label: 'SchoolList', path: '/schools' },
+    { label: 'SchoolDetails', path: 'schools/3' },
+    { label: 'Login', path: '/login' },
+    { label: 'Register', path: '/register' },
+    { label: 'UserList', path: '/users' },
+    { label: 'UserDetails', path: 'users/3' },
+    { label: 'Dashboard', path: '/admin' },
+    { label: 'UserProfile', path: '/users/3/profile' },
+  ];
   return (
     <>
+      {pages.map((page, index) => (
+        <Button
+          key={index}
+          to={page.path}
+          component={Link}
+          variant="outlined"
+          sx={{ mx: 1 }}
+        >
+          {page.label}
+        </Button>
+      ))}
+
       <Routes>
         {/* public routes */}
         <Route path="/" element={<Home />} />
