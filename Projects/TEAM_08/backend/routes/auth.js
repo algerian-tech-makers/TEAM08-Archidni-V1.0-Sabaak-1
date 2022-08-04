5d858ed731c26eaed67d0f65dedbb76ad19233cb
proXDhiya <prox.dhiya@gmail.com>
2022-08-04 13:02:15 +0100

    add decode-token & delete logout

--- Projects/TEAM_08/backend/routes/auth.js
+++ Projects/TEAM_08/backend/routes/auth.js
@@ -6,8 +6,9 @@ const router = express.Router();
 // add api routes
 router.post('/login', require('../controllers/login').login);
 router.post('/signup', require('../controllers/signup').signup );
-router.get('/logout', (req, res) => { res.status(200).json({ message: 'logout' }); });
 router.post('/refresh', (req, res) => { res.status(200).json({ message: 'refresh' }); });
+router.post('/decode-token', require('../controllers/decode-token').decodeToken);
+
 
 // export router
 module.exports = router;    
