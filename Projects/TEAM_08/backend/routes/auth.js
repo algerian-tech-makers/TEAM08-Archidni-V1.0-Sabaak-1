04bd36d5cd2361e138f5a258e413ad54924f60df
proXDhiya <prox.dhiya@gmail.com>
2022-08-04 13:30:18 +0100

    delete refresh token

--- Projects/TEAM_08/backend/routes/auth.js
+++ Projects/TEAM_08/backend/routes/auth.js
@@ -6,7 +6,6 @@ const router = express.Router();
 // add api routes
 router.post('/login', require('../controllers/login').login);
 router.post('/signup', require('../controllers/signup').signup );
-router.post('/refresh', (req, res) => { res.status(200).json({ message: 'refresh' }); });
 router.post('/decode-token', require('../controllers/decode-token').decodeToken);
 
 
