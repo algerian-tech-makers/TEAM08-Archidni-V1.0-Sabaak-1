2fd7d4ee67dfa3377d119aec029e1517af6cfae8
proXDhiya <prox.dhiya@gmail.com>
2022-08-07 00:06:09 +0100

    update :) ;

--- Projects/TEAM_08/backend/routes/auth.js
+++ Projects/TEAM_08/backend/routes/auth.js
@@ -6,6 +6,7 @@ const router = express.Router();
 // add api routes
 router.post('/login', require('../controllers/login').login);
 router.post('/signup', require('../controllers/signup').signup );
+// router.delete('/delete', require('../controllers/delete').delete);
 router.post('/decode-token', require('../controllers/decode-token').decodeToken);
 
 
